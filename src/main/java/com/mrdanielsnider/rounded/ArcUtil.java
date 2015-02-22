77d19a584864ee407c8f42b3b2d4f10c74c28716
Daniel Snider <qtipkk@gmail.com>
2015-02-22 02:13:30 -0800

    initial commit

---
+++ src/main/java/com/mrdanielsnider/rounded/ArcUtil.java
@@ -0,0 +1,68 @@
+package com.mrdanielsnider.rounded;
+
+public class ArcUtil {
+    private static double solve1(double a, double b, double c, double d, double e, double f) {
+        return (c * e - b * f) / (a * e - b * d);
+    }
+
+    private static double solve2(double a, double b, double c, double d, double e, double f) {
+        return (c * d - a * f) / (b * d - a * e);
+    }
+
+    public static Arc roundedEdge(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4, double radius) {
+
+        // compute to normal direction vectors for each of the given lines
+        double mag_a = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        double mag_b = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+        double vec_a_x = (x2 - x1) / mag_a;
+        double vec_a_y = (y2 - y1) / mag_a;
+        double vec_b_x = (x4 - x3) / mag_b;
+        double vec_b_y = (y4 - y3) / mag_b;
+
+        // bisect the given lines at their intersection
+        double bisect_x = vec_a_x + vec_b_x;
+        double bisect_y = vec_a_y + vec_b_y;
+
+        // flip the bisector so the output circle is inset
+        bisect_x *= -1;
+        bisect_y *= -1;
+
+        // find the point where the given lines intersect
+        double t = solve1(vec_a_x, -vec_b_x, x3 - x1, vec_a_y, -vec_b_y, y3 - y1);
+        double intercept_x = x1 + t * vec_a_x;
+        double intercept_y = y1 + t * vec_a_y;
+
+        // find the two points tangent to the circle that intersect the given lines
+        double u = solve1(vec_a_x, -bisect_x, radius * vec_a_y, vec_a_y, -bisect_y, radius * -vec_a_x);
+        double tangent_a_x = -Math.abs(u) * vec_a_x + intercept_x;
+        double tangent_a_y = -Math.abs(u) * vec_a_y + intercept_y;
+        double tangent_b_x = -Math.abs(u) * vec_b_x + intercept_x;
+        double tangent_b_y = -Math.abs(u) * vec_b_y + intercept_y;
+
+        // find a center point for a circle with the given radius, such that the given lines are both tangent to it
+        double v = solve2(vec_a_x, -bisect_x, radius * vec_a_y, vec_a_y, -bisect_y, radius * -vec_a_x);
+        double center_x = Math.abs(v) * bisect_x + intercept_x;
+        double center_y = Math.abs(v) * bisect_y + intercept_y;
+
+        // compute the angle distance between the two tangent points
+        double clock_a_x = tangent_a_x - center_x;
+        double clock_a_y = tangent_a_y - center_y;
+        double clock_b_x = tangent_b_x - center_x;
+        double clock_b_y = tangent_b_y - center_y;
+        double arcAngle = Math.acos((clock_a_x * clock_b_x + clock_a_y * clock_b_y) / (radius * radius)) * 180.0 / Math.PI;
+
+        return new Arc(center_x, center_y, tangent_a_x, tangent_a_y, arcAngle);
+    }
+
+    public static class Arc {
+        public double centerX, centerY, startX, startY, arcAngle;
+
+        public Arc(double centerX, double centerY, double startX, double startY, double arcAngle) {
+            this.centerX = centerX;
+            this.centerY = centerY;
+            this.startX = startX;
+            this.startY = startY;
+            this.arcAngle = arcAngle;
+        }
+    }
+}
---
+++ src/main/java/com/mrdanielsnider/rounded/RoundedViewer.java
@@ -0,0 +1,128 @@
+package com.mrdanielsnider.rounded;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class RoundedViewer extends JPanel {
+    private double radius;
+    private double[] vectors = new double[8];
+    private int draggingVector;
+
+    private boolean isDragging() {
+        return draggingVector != -1;
+    }
+
+    private void clearDragging() {
+        draggingVector = -1;
+    }
+
+    private void setDragging(int i) {
+        draggingVector = i;
+    }
+
+    public RoundedViewer(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4, double radius) {
+        vectors = new double[]{x1, y1, x2, y2, x3, y3, x4, y4};
+        this.radius = radius;
+        clearDragging();
+        MouseAdapter mouseAdapter = new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                super.mousePressed(e);
+                for (int i = 0; i < vectors.length / 2; i++) {
+                    double x = vectors[i * 2];
+                    double y = vectors[i * 2 + 1];
+                    double sqrDist = (e.getX() - x) * (e.getX() - x) + (e.getY() - y) * (e.getY() - y);
+                    if (sqrDist <= 50 * 50) {
+                        setDragging(i);
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                super.mouseReleased(e);
+                if (isDragging()) {
+                    vectors[draggingVector * 2] = e.getX();
+                    vectors[draggingVector * 2 + 1] = e.getY();
+                    clearDragging();
+                    repaint();
+                }
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                super.mouseDragged(e);
+                if (isDragging()) {
+                    vectors[draggingVector * 2] = e.getX();
+                    vectors[draggingVector * 2 + 1] = e.getY();
+                    repaint();
+                }
+            }
+        };
+        addMouseListener(mouseAdapter);
+        addMouseMotionListener(mouseAdapter);
+
+    }
+
+    void drawSegment(Graphics2D g, double x1, double y1, double x2, double y2) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g.drawLine((int) x1, (int) y1, (int) x2, (int) y2);
+        g2.setColor(Color.BLACK);
+        drawPoint(g2, x1, y1);
+        g2.setColor(Color.WHITE);
+        drawPoint(g2, x2, y2);
+        g2.dispose();
+    }
+
+    void drawPoint(Graphics2D g, double x, double y) {
+        g.fillOval((int) x - 4, (int) y - 4, 8, 8);
+    }
+
+
+    @Override
+    public void paint(Graphics graphics) {
+        super.paint(graphics);
+
+        Graphics2D g = (Graphics2D) graphics;
+        drawSegment(g, vectors[0], vectors[1], vectors[2], vectors[3]);
+        drawSegment(g, vectors[4], vectors[5], vectors[6], vectors[7]);
+
+        double radius = 20.0;
+        ArcUtil.Arc arc = ArcUtil.roundedEdge(vectors[0], vectors[1], vectors[2], vectors[3], vectors[4], vectors[5], vectors[6], vectors[7], radius);
+
+        drawPoint(g, arc.startX, arc.startY);
+        g.drawOval((int) (arc.centerX - radius), (int) (arc.centerY - radius), (int) (2 * radius), (int) (2 * radius));
+
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setStroke(new BasicStroke(4));
+        double startAngle = Math.acos((arc.startX - arc.centerX) / radius) * 180.0 / Math.PI;
+        g2.drawArc((int) (arc.centerX - radius), (int) (arc.centerY - radius), (int) (2 * radius), (int) (2 * radius), (int) startAngle, (int) arc.arcAngle);
+        g2.dispose();
+
+        g.setFont(Font.getFont(Font.MONOSPACED));
+        g.drawString("centerX:  " + arc.centerX, 100, 100);
+        g.drawString("centerY:  " + arc.centerY, 100, 115);
+        g.drawString("startX:   " + arc.startX, 100, 130);
+        g.drawString("startY:   " + arc.startY, 100, 145);
+        g.drawString("arcAngle: " + arc.arcAngle, 100, 160);
+
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(800, 600);
+    }
+
+    public static void main(String[] args) {
+        RoundedViewer roundedViewer2 = new RoundedViewer(500, 300, 300, 300, 400, 400, 400, 200, 20);
+        JFrame frame = new JFrame();
+        frame.add(roundedViewer2);
+        frame.pack();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+}
